dc9c143a105387f183162b6cf226d27fe652f758
Wrathster2103 <51835529+wrathster2103@users.noreply.github.com>
2025-11-08 18:20:31 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Wrathster2103'
+description 'Changelog poster to Discord via webhook'
+version '1.0.0'
+
+server_scripts {
+    'server.lua',
+    'changelog.json'
+}
+
+-- Add this resource to your server.cfg with: start changelog_poster
